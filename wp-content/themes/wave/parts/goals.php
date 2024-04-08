8d5c56b7733ff5d27a6679f72c39acf6be672e10
Clint Eric Mosqueda <ericmosqueda1991@gmail.com>
2024-04-08 11:28:38 +0800

    update goals section

--- wp-content/themes/wave/parts/goals.php
+++ wp-content/themes/wave/parts/goals.php
@@ -1,3 +1,25 @@
 <div class="goals animate-in fade-in">
-  <img src="<?php echo resolve_asset_url('/images/about-wave.jpg'); ?>" alt="">
+  <!-- <img src="<?php echo resolve_asset_url('/images/about-wave.jpg'); ?>" alt=""> -->
+  <div class="goals-content">
+    <div class="goals-about">
+      <?php import_part('heading', array(
+        'modifier' => 'goals-heading',
+        'title_en' => 'About Wave',
+        'title_jp' => '会社概要',
+      ));?>
+      <p class="goals-desc">会社名 株式会社 WAVE CREATIVE
+      代表者 成田 淳
+      所在地 106-0032
+      東京都，港区六本木7-11-18
+      サンフラット六本木 301
+      設立 2021年9月30日
+      資本金 300万</p>
+    </div>
+    <div class="goals-figure">
+      <?php import_part('image', array(
+        'modifier' => 'goals-development',
+        'src' => '/images/development-goals.jpg'
+      ))?>
+    </div>
+  </div>
 </div>
