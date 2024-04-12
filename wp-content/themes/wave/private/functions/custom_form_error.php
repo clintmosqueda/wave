8ce0d560a7586e482190ed2f22578d0462fb8808
Clint Eric Mosqueda <ericmosqueda1991@gmail.com>
2024-04-13 07:53:26 +0800

    compress image

--- wp-content/themes/wave/private/functions/custom_form_error.php
+++ wp-content/themes/wave/private/functions/custom_form_error.php
@@ -3,6 +3,18 @@
 function contact_form_error_message( $error, $key, $rule ) {
   switch ( $key ) {
 
+    case 'name':
+      $error = 'お名前を入力してください';
+      break;
+
+    case 'email':
+      $error = 'メールアドレスを入力してください';
+      break;
+
+    case 'inquiry_type':
+      $error = 'お問合せ種別を入力してください';
+      break;
+
     default:
       $error = '入力してください';
       break;
@@ -11,4 +23,5 @@ function contact_form_error_message( $error, $key, $rule ) {
   return $error;
 }
 
+add_filter( 'mwform_error_message_mw-wp-form-66', 'contact_form_error_message', 10, 3 );
 add_filter( 'mwform_error_message_mw-wp-form-60', 'contact_form_error_message', 10, 3 );
